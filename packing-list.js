b1c3d31162d2cb93f89a180898aa6e5de815e17b
Samreen Ahmed <neermas1@hotmail.com>
2022-11-03 21:26:56 -0400

    Modified with a while loop

--- packing-list.js
+++ packing-list.js
@@ -7,9 +7,9 @@ for (let i = 0; i < packingList.length; i++) {
 }
 */
 
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
